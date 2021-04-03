d92348c9ad89315d4952c3a431d49bace1259860
Saul Maldonado <saul.maldonado@gmail.com>
2021-04-03 17:35:45 +0200

    adding exchange and token address

--- contracts/Flashloan.sol
+++ contracts/Flashloan.sol
@@ -3,8 +3,11 @@ pragma experimental ABIEncoderV2;
 
 import "@studydefi/money-legos/dydx/contracts/DydxFlashloanBase.sol";
 import "@studydefi/money-legos/dydx/contracts/ICallee.sol";
+import { KyberNetworkProxy as IKyberNetworkProxy } from '@studydefi/money-legos/kyber/contracts/KyberNetworkProxy.sol';
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./IUniswapV2Router02.sol";
+import "./IWeth.sol";
 
 // Inheriting from ICallee, DydxFlashloanBase smart contracts to have basic functionalities
 contract Flashloan is ICallee, DydxFlashloanBase {
@@ -16,6 +19,27 @@ contract Flashloan is ICallee, DydxFlashloanBase {
         uint256 repayAmount;
     }
 
+    // Defining exchanges and addresses pointers for smart contracts
+    IKyberNetworkProxy kyber;
+    IUniswapV2Router02 uniswap;
+    IWeth weth;
+    IERC20 dai;
+    // This is constant value to deal with ETH in Kyber
+    address constant KYBER_ETH_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    // This constructor is a special function that will be called when the smart contract is deployed to the blockchain
+    constructor(
+        address kyberAddress,
+        address uniswapAddress,
+        address weithAddress,
+        address daiAddress
+    ) public {
+        kyber = IKyberNetworkProxy(kyberAddress);
+        uniswap = IUniswapV2Router02(uniswapAddress);
+        weth = IWeth(weithAddress);
+        dai = IERC20(daiAddress);
+    }
+
     // This is the function that will be called postLoan
     // i.e. Encode the logic to handle your flashloaned funds here
     function callFunction(
@@ -24,7 +48,15 @@ contract Flashloan is ICallee, DydxFlashloanBase {
         bytes memory data
     ) public {
         ArbInfo memory arbInfo = abi.decode(data, (ArbInfo));
+        // getting dai balance
+        unit256 balanceDai = dai.balanceOf(address(this));
 
+        // IMPORTANT: this require section is where the contract is valid.
+        //            if something fails then the contract will fial.
+        require(
+            balanceDai >= arbInfo.repayAmount,
+            "Not enough funds to repay DyDx loan"
+        );
     }
 
     function initiateFlashLoan(
